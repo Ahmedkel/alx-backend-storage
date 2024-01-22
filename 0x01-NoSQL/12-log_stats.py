31808121ce940af17126de032234933c486923bf
Ahmedkel <ahmedkel@gmail.com>
2024-01-22 18:12:18 +0100

    new code

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,24 +3,20 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
+def print_stats(mongo_collection):
+    """print some stats about Nginx logs stored in MongoDB"""
+    total_logs = mongo_collection.count_documents({})
+    print(f'{total_logs} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-
-db = client.logs
-collection = db.nginx
-
-
-total_logs = collection.count_documents({})
-print(f"{total_logs} logs")
-
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    count = collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
-
-status_check = collection.count_documents({"method": "GET", "path": "/status"})
-print(f"{status_check} status check")
+    status_check_count = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+    
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
