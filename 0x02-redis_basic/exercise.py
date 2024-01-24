610a9f50cfba3e0fe1e8e46768525fe20ad22d1b
Ahmedkel <ahmedkel@gmail.com>
2024-01-24 16:05:34 +0100

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""this is module for redis"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """main class for redis"""
+    def __init__(self):
+        """init method for redis"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """sotre method for redis"""
+        key = str(uuid.uuid4())
+        if isinstance(data, str):
+            self._redis.set(key, data)
+        elif isinstance(data, bytes):
+            self._redis.set(key, data)
+        elif isinstance(data, int):
+            self._redis.set(key, str(data))
+        elif isinstance(data, float):
+            self._redis.set(key, str(data))
+        return key
